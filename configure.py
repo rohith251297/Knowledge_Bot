a89d93103f1f208a3842d8ce273c5f685d32db75
rohith <126263351+rohith251297@users.noreply.github.com>
2025-07-14 09:29:07 +0530

    Update configure.py

--- configure.py
+++ configure.py
@@ -12,10 +12,10 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 INPUT_FOLDER = r"R:\Knowledge_Bot\documents"
 
 # Output base directory for images extracted from PDFs (optional if you need later)
-OUTPUT_BASE = os.path.join(os.getcwd(), "figures")
+OUTPUT_BASE = os.path.join(os.getcwd(), "images")
 
 # Chroma Vector Store persistent directory
-CHROMA_DB_DIR = r"R:\New folder (11)\chroma_db"
+CHROMA_DB_DIR = r"R:\Knowledge_Bot\chroma_db"
 
 # Chroma collection name
 COLLECTION_NAME = "summaries"
